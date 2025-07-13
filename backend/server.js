2955fba71d642cad98af253968093845133f9364
OlaRedaAbdulrazeq <olaabdelrazeq@std.mans.edu.eg>
2025-07-13 23:47:05 +0300

    initial setup and commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/user.route');
+const adminRoutes = require('./routes/admin.route');
+const unitRouter = require('./routes/unit.route')
+const path = require('path');
+const httpStatusText = require("./utils/httpStatusText");
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+// Serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/units', unitRouter)
+
+//global error handler
+//route=>controller name=>asyncwrapper returns function which calls actual controller function with next and then passes it here
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    status: error.StatusText || httpStatusText.ERROR,
+    message: error.message,
+    code: error.statusCode || 500,
+    data: null,
+  });
+});
+
+// 404 handler
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
