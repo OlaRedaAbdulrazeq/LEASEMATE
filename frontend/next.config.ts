238b8c2e981b8365511fdab38b143dc09dd71f0c
rhf007 <rahofahazem512@gmail.com>
2025-07-23 19:03:35 +0300

    next img config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    //TODO: (optional) remove unsplash
+    domains: ["res.cloudinary.com", "images.unsplash.com"],
   },
 };
 
