e52c16dad5b96a9fd45a10de10c758efdeab3df0
NadeenElGendi <nadineamrelgendi@gmail.com>
2025-07-23 14:11:19 +0300

    Initial commit from ZIP

---
+++ backend/routes/booking.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const bookingController = require("../controllers/booking.controller");
+const {  protect } = require("../middlewares/auth.middleware");
+const { checkRole } = require("../middlewares/role.middleware");
+
+// إرسال طلب حجز (للمستأجر)
+router.post("/request", protect, bookingController.createBookingRequest);
+
+// جلب طلبات الحجز للمالك
+router.get("/landlord-requests", protect, checkRole("landlord"), bookingController.getLandlordBookings);
+
+// حذف طلب الإيجار من قبل المالك
+router.delete("/request/:id/reject", protect, checkRole("landlord"), bookingController.rejectBookingRequest);
+
+module.exports = router; 
